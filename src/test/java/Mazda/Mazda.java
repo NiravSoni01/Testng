9d2d13dc81ebe377e7dc3ecd8a76616debbfbb6d
nirav.soni <nirav.soni@TOR7JB7ZD3.na.corp.jwt.com>
2023-10-04 21:26:22 -0400

    WIndow max

--- src/test/java/Mazda/Mazda.java
+++ src/test/java/Mazda/Mazda.java
@@ -25,7 +25,7 @@ public void firsttest() throws Throwable {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
         driver.get("https://mazda.ca/en/");
-        
+        driver.manage().window().setSize(new Dimension(1440, 900));
        // driver.manage().window().maximize();
 		
 		Thread.sleep(1000);
